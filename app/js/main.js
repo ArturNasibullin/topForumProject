d935e6058c4d3b5ba3931d9820038ffefb269c2d
Artur Nasibullin <nasibullin.artur@gmail.com>
2021-01-27 14:33:54 +0500

    add responsive tablet(768px) view

--- app/js/main.js
+++ app/js/main.js
@@ -1,30 +1,73 @@
 new Glider(document.querySelector('.hero-slider__wrap'), {
 	slidesToShow: 1,
-	draggable: true,
 	rewind: true,
+	draggable: true,
+	dots: '.hero-dots',
 	arrows: {
-		prev: '.hero-slider__arrows-left',
-		next: '.hero-slider__arrows-right'
-	}
+		prev: '.hero-slider-arrows__left',
+		next: '.hero-slider-arrows__right'
+	},
+	responsive: [
+		{
+		  // screens greater than >= 775px
+		  breakpoint: 1024,
+		  settings: {
+			draggable: false,
+		  }
+		}
+	  ]
 });
 
 window.addEventListener('load', function () {
 	new Glider(document.querySelector('.review-slider__wrap'), {
-		slidesToShow: 2,
+		slidesToShow: 1,
+  		slidesToScroll: 1,
+  		scrollLock: true,
+  		dots: '.review-dots',
 		draggable: true,
-		rewind: true,
 		arrows: {
 			prev: '.review-item__arrows-left',
 			next: '.review-item__arrows-right'
-		}
+		},
+		responsive: [
+			{
+			  // screens greater than >= 775px
+			  breakpoint: 768,
+			  settings: {
+				slidesToShow: 2,
+				draggable: false,
+				dots: '.review-dots',
+				rewind: true
+			  }
+			}
+		  ]
 	});
 	new Glider(document.querySelector('.clients-slider__wrap'), {
-		slidesToShow: 6,
+		slidesToShow: 3,
+		slidesToScroll: 1,
 		draggable: true,
-		rewind: true,
 		arrows: {
 			prev: '.clients-item__arrows-left',
 			next: '.clients-item__arrows-right'
-		}
+		},
+		responsive: [
+			{
+			  // screens greater than >= 775px
+			  breakpoint: 768,
+			  settings: {
+				slidesToShow: 6,
+				draggable: false,
+				autoplay: true,
+				rewind: true
+			  }
+			},
+			{
+				// // screens greater than >= 1024px
+				// breakpoint: 1024,
+				// settings: {
+				//   slidesToShow: 6
+				// }
+			}
+		  ]
 	});
-})
+});
